097c201a491f80c08969660b3c35e646019025da
Fabrício M. Damasceno <fabricio.damasceno@outlook.com.br>
2022-10-11 20:05:44 -0300

    Adicionando Classes

---
+++ Web/Author.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    public class Author
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Web/Query.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    public class Query
+    {
+        public Book GetBook() =>
+        new Book
+        {
+            Title = "C# in depth.",
+            Author = new Author
+            {
+                Name = "Jon Skeet"
+            }
+        };
+    }
+}
