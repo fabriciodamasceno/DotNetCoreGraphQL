d47316c25b4f46f8916d69b9258cbd8f8b2a854a
Fabrício M. Damasceno <fabricio.damasceno@outlook.com.br>
2022-10-11 20:06:40 -0300

    Adicionando GraphQL ao Startup

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -16,6 +16,9 @@ namespace Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddGraphQLServer()
+                .AddQueryType<Query>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +33,7 @@ namespace Web
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapGraphQL();
             });
         }
     }
